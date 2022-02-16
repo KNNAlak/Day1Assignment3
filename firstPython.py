12d483c38afbcdcb0f0e4400148bbb79815c56e1
Hassan Alijani <hassanalijani@gmail.com>
2022-02-16 01:36:51 -0500

    Adding an initial python code

---
+++ firstPython.py
@@ -0,0 +1,4 @@
+length = float(input('Please input the length of the room: '))
+width = float(input('Please input the Width of the room: '))
+print(f'room area that need to be covered: ', length*width)
+print(f'room perimeter that need to be installed baseboard :', 2*(length+width))
